29858ff8c29fc1fe149b06fb73150e73274f8b2a
faroksalman <136682886+faroksalman@users.noreply.github.com>
2023-07-30 18:19:37 +0300

    first commit

---
+++ js8.1.js
@@ -0,0 +1,7 @@
+const genreToFind = "Romance";
+  const movieWithGenre = findMovieByGenre(movies, genreToFind);
+  console.log(movieWithGenre);
+  // Output: { title: "Movie 2", genres: ["Drama", "Romance"] }
+
+
+  
